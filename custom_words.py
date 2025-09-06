061f26a7d7c4ce2f55907186ec9f2373b1afc834
DinoMaksumicGGC <dmaksumic@ggc.edu>
2025-09-06 14:54:46 -0400

    Final edit ready for presentation

--- custom_words.py
+++ custom_words.py
@@ -1,7 +1,16 @@
 import random
 
 answer_directory = {
-'Ella#197171': {'player':'Class', 'theme':'hacking lindas facebook password', 'hint':'Numbers and one special character'},
+'Ella#197171': {
+        'player': 'Class',
+        'theme': 'hacking lindas facebook password',
+        'hint': "Linda loves her 2 grandkids (Ella & Mason)\n"
+                "Linda is a huge Braves baseball fan\n"
+                "Her birthday is July 15, 1971\n"
+                "She uses Facebook and checks email\n"
+                "but isn't very tech-savvy.\n"
+                "Hint: Numbers and one special character are included."
+    }
 
 }
 keys = list(answer_directory.keys())
